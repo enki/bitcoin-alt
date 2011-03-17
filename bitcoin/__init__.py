901429108ad75e72b0dc76a22fc35376e6d8f94b
phantomcircuit <phantomcircuit@covertinferno.org>
2011-03-17 09:49:28 -0700

    moved data objects to higher level

---
+++ bitcoin/__init__.py
@@ -0,0 +1,30 @@
+class Block(object):
+  def __init__(self,block_hash,prev_hash,merkle_root,timestamp,bits,nonce,version):
+    self.hash = block_hash
+    self.prev_hash = prev_hash
+    self.merkle_root = merkle_root
+    self.timestamp = timestamp
+    self.bits = bits
+    self.nonce = none
+    self.version = version
+    
+class Tx(object): 
+  def __init__(self,tx_hash,sequence,version,lock_time):
+    self.hash = tx_hash
+    self.sequence = sequence
+    self.version = version
+    self.lock_time = lock_time
+    
+    self.tx_ins = []
+    self.tx_outs = []
+
+class TxOut(object): 
+  def __init__(self,value,script):
+    self.value = value
+    self.script = script
+  
+class TxIn(object):  
+  def __init__(self,out_hash,out_index,script):
+    self.out_hash = out_hash
+    self.out_index = out_index
+    self.script = script
--- bitcoin/storage.py
+++ bitcoin/storage.py
@@ -1,31 +1,71 @@
-import sqlite3
-import threading
-import hashlib
-import time
-
+import bitcoin
 import bitcoin.net.payload
 
+from sqlalchemy import create_engine,Table,Column,MetaData,ForeignKey,DateTime,Integer,BigInteger,SmallInteger,Float
+from sqlalchemy.types import BINARY
+from sqlalchemy.orm import mapper,relationship
+from sqlalchemy.ext.orderinglist import ordering_list
+
+engine = create_engine('sqlite:///bitcoin.sqlite3', echo=True)
+metadata = MetaData()
+
+blocks_table = Table('blocks',metadata,
+  Column('id',Integer,primary_key=True),
+  Column('hash',BINARY(32),unique=True),
+  Column('prev_hash',BINARY(32),unique=True),
+  Column('merkle_root',BINARY(32)),
+  Column('timestamp',DateTime,index=True),
+  Column('bits',Integer,index=True),
+  Column('nonce',BINARY(8)),
+  Column('version',SmallInteger),
+  Column('height',Float,nullable=True,index=True),
+)
+
+txs_table = Table('txs',metadata,
+  Column('id',Integer,primary_key=True),
+  Column('hash',BINARY(32),index=True),
+  Column('sequence',Integer,index=True),
+  Column('version',SmallInteger),
+  Column('lock_time',DateTime,index=True),
+  Column('block_id',Integer,ForeignKey('blocks.id'),nullable=True),
+)
+
+tx_ins_table = Table('tx_ins',metadata,
+  Column('id',Integer,primary_key=True),
+  Column('out_hash',BINARY(32),index=True),
+  Column('out_index',Integer),
+  Column('script',BINARY),
+  Column('sequence',Integer),
+  Column('tx_id',Integer,ForeignKey('txs.id')),
+)
+
+tx_outs_table = Table('tx_outs',metadata,
+  Column('id',Integer,primary_key=True),
+  Column('value',BigInteger,index=True),
+  Column('script',BINARY),
+  Column('sequence',Integer),
+  Column('tx_id',Integer,ForeignKey('txs.id')),
+)
+
+metadata.create_all(engine)
+
+mapper(bitcoin.Block,blocks_table,properties={
+  'prev_block': relationship(bitcoin.Block,primaryjoin=blocks_table.c.hash==blocks_table.c.prev_hash,remote_side=blocks_table.c.prev_hash),
+  'txs': relationship(bitcoin.Tx,order_by=[txs_table.c.sequence],collection_class=ordering_list('sequence')),
+})
+mapper(bitcoin.Tx,txs_table,properties={
+  'tx_ins': relationship(bitcoin.TxIn,order_by=[tx_ins_table.c.sequence],collection_class=ordering_list('sequence')),
+  'tx_outs': relationship(bitcoin.TxOut,order_by=[tx_outs_table.c.sequence],collection_class=ordering_list('sequence')),
+})
+mapper(bitcoin.TxOut,tx_outs_table,properties={
+})
+mapper(bitcoin.TxIn,tx_ins_table,properties={
+})
+
 class Storage:
-  genesis_block = {'height': 1.0, 'nonce': b'\x1d\xac+|', 'version': 1, 'hash': b'o\xe2\x8c\n\xb6\xf1\xb3r\xc1\xa6\xa2F\xaec\xf7O\x93\x1e\x83e\xe1Z\x08\x9ch\xd6\x19\x00\x00\x00\x00\x00', 'txs': [{'tx_outs': [{'pk_script': b"A\x04g\x8a\xfd\xb0\xfeUH'\x19g\xf1\xa6q0\xb7\x10\\\xd6\xa8(\xe09\t\xa6yb\xe0\xea\x1fa\xde\xb6I\xf6\xbc?L\xef8\xc4\xf3U\x04\xe5\x1e\xc1\x12\xde\\8M\xf7\xba\x0b\x8dW\x8aLp+k\xf1\x1d_\xac", 'value': 5000000000}], 'lock_time': 0, 'version': 1, 'hash': None, 'tx_ins': [{'sequence': 4294967295, 'outpoint': {'out_index': 4294967295, 'out_hash': b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'}, 'script': b'\x04\xff\xff\x00\x1d\x01\x04EThe Times 03/Jan/2009 Chancellor on brink of second bailout for banks'}]}], 'prev_hash': b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00', 'timestamp': 1231006505, 'merkle_root': b';\xa3\xed\xfdz{\x12\xb2z\xc7,>gv\x8fa\x7f\xc8\x1b\xc3\x88\x8aQ2:\x9f\xb8\xaaK\x1e^J', 'bits': 486604799}
-  
-  create_stmts = [
-  """CREATE TABLE IF NOT EXISTS blocks(hash, prev_hash, merkle_root, timestamp, bits, nonce, version, height);""",
-  """CREATE TABLE IF NOT EXISTS txs(hash, block, sequence, version, lock_time);""",
-  """CREATE TABLE IF NOT EXISTS tx_ins(tx, out_hash, out_index, script, sequence);""",
-  """CREATE TABLE IF NOT EXISTS tx_outs(tx, value, script);""",
-  """CREATE UNIQUE INDEX IF NOT EXISTS blocks_hash ON blocks(hash)""",
-  """CREATE UNIQUE INDEX IF NOT EXISTS blocks_prev_hash ON blocks(prev_hash)""",
-  """CREATE UNIQUE INDEX IF NOT EXISTS blocks_height ON blocks(height)""",
-  """CREATE UNIQUE INDEX IF NOT EXISTS txs_hash ON txs(hash)""",
-  ]
+  genesis_hash = b'o\xe2\x8c\n\xb6\xf1\xb3r\xc1\xa6\xa2F\xaec\xf7O\x93\x1e\x83e\xe1Z\x08\x9ch\xd6\x19\x00\x00\x00\x00\x00'    
   
   def __init__(self):
-    self.db = sqlite3.connect('bitcoin.sqlite3')
-    self.db.row_factory = sqlite3.Row
-    
-    for create_stmt in Storage.create_stmts:
-      self.db.execute(create_stmt)
-    self.db.commit()
     
     self.dlock = threading.RLock()
     self.tx_cache = {}
